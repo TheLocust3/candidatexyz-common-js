2af5184354b446aff531e7e0c59b4dd1417a5c72
TheLocust3 <jake.kinsella@gmail.com>
2018-06-28 16:20:19 -0400

    Remove send to contacts

--- src/api/mail-api.js
+++ src/api/mail-api.js
@@ -1,29 +1,6 @@
-import { volunteerApi, mailerApi } from '../helpers';
+import { mailerApi } from '../helpers';
 
 let MailApi = {
-
-    sendToContacts(subject, body) {
-        let fullBody = `
-            ${body}
-
-            <div class='footer'>
-                <center>
-                    Reading Democratic Committee<br /><br />
-
-                    If you believe you received this message in error or wish to no longer receive email from us, please <a href='[UNSUBSCRIBE]'>unsubscribe</a>.
-                </center>
-            </div>
-        `;
-
-        return new Promise((resolve, reject) => {
-            volunteerApi('/contacts/send_email', {
-                type: 'post',
-                data: { subject: subject, body: fullBody },
-                success: resolve,
-                error: reject
-            });
-        });
-    },
 
     sendEmail(email, subject, body, resourceType, resourceId) {
         let fullBody = `
