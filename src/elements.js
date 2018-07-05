0ac983ca4e5d33b46a408e84fc6e18d98cfb525b
TheLocust3 <jake.kinsella@gmail.com>
2018-07-05 13:05:26 -0400

    Move elements/index to elements

---
+++ src/elements.js
@@ -0,0 +1,19 @@
+import ButtonElement from './elements/Button';
+import CheckboxElement from './elements/Checkbox';
+import FormElement from './elements/Form';
+import SelectElement from './elements/Select';
+import SelectItemElement from './elements/SelectItem';
+import TextElement from './elements/Text';
+import TextAreaElement from './elements/TextArea';
+import TextEditorElement from './elements/TextEditor';
+import TextFieldElement from './elements/TextField';
+
+export let Button = ButtonElement;
+export let Checkbox = CheckboxElement;
+export let Form = FormElement;
+export let Select = SelectElement;
+export let SelectItem = SelectItemElement;
+export let Text = TextElement;
+export let TextArea = TextAreaElement;
+export let TextEditor = TextEditorElement;
+export let TextField = TextFieldElement;
