95f967c1d70da476d5d64580395c3ab7c197ec4e
TheLocust3 <jake.kinsella@gmail.com>
2018-07-12 15:40:56 -0400

    Update receipts

--- src/api/receipt-api.js
+++ src/api/receipt-api.js
@@ -22,22 +22,22 @@ let ReceiptApi = {
         });
     },
 
-    create(name, receiptType, amount, address, zipcode, city, state, dateReceived, occupation, employer, email, phoneNumber) {
+    create(name, receiptType, amount, address, city, state, country, dateReceived, occupation, employer, email, phoneNumber) {
         return new Promise((resolve, reject) => {
             volunteerApi('/receipts', {
                 type: 'post',
-                data: { name: name, receipt_type: receiptType, amount: amount, address: address, zipcode: zipcode, city: city, state: state, date_received: dateReceived, occupation: occupation, employer: employer, email: email, phone_number: phoneNumber },
+                data: { name: name, receipt_type: receiptType, amount: amount, address: address, city: city, state: state, country: country, date_received: dateReceived, occupation: occupation, employer: employer, email: email, phone_number: phoneNumber },
                 success: resolve,
                 error: reject
             });
         });
     },
 
-    update(id, name, receiptType, amount, address, zipcode, city, state, dateReceived, occupation, employer, email, phoneNumber) {
+    update(id, name, receiptType, amount, address, city, state, country, dateReceived, occupation, employer, email, phoneNumber) {
         return new Promise((resolve, reject) => {
             volunteerApi(`/receipts/${id}`, {
                 type: 'patch',
-                data: { name: name, receipt_type: receiptType, amount: amount, address: address, zipcode: zipcode, city: city, state: state, date_received: dateReceived, occupation: occupation, employer: employer, email: email, phone_number: phoneNumber },
+                data: { name: name, receipt_type: receiptType, amount: amount, address: address, zipcode: zipcode, city: city, state: state, country: country, date_received: dateReceived, occupation: occupation, employer: employer, email: email, phone_number: phoneNumber },
                 success: resolve,
                 error: reject
             });
