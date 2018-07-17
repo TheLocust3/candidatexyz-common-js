1591244adf660543aab6b96724e82917ce991526
TheLocust3 <jake.kinsella@gmail.com>
2018-07-17 09:27:24 -0400

    Add generate report type

---
+++ src/helpers/report-helper.js
@@ -0,0 +1,31 @@
+import moment from 'moment';
+
+let ReportHelper = {
+    
+    generateReportType(reportType, campaign) {
+        let newReportType = _.clone(reportType);
+
+        let baseDate = campaign[reportType.type];
+        if (_.isEmpty(baseDate)) {
+            if (_.isEmpty(reportType.dueDate)) { // dissolution, no dates attached
+                return newReportType;
+            }
+
+            let year = moment().year();
+            if (moment().month() == 1) {
+                newReportType.dueDate = moment(newReportType.dueDate).year(year);
+                newReportType.endingDate = moment(newReportType.endingDate).year(year - 1);
+            } else {
+                newReportType.dueDate = moment(newReportType.dueDate).year(year + 1);
+                newReportType.endingDate = moment(newReportType.endingDate).year(year);
+            }
+        } else {
+            newReportType.dueDate = moment(baseDate).add(Number(reportType.dueDate), 'days');
+            newReportType.endingDate = moment(baseDate).add(Number(reportType.endingDate), 'days');
+        }
+
+        return newReportType;
+    }
+}
+
+export default ReportHelper;
