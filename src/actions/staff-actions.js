b242156c50cb78189e249761717ab0542135bff2
TheLocust3 <jake.kinsella@gmail.com>
2018-07-19 10:56:15 -0400

    Fix bug

--- src/actions/staff-actions.js
+++ src/actions/staff-actions.js
@@ -113,7 +113,7 @@ export function fetchInvitedUser(token) {
     return function (dispatch) {
         dispatch(requestUser());
 
-        StaffApi.get_invite(token).then( data => {
+        StaffApi.getInvite(token).then( data => {
             dispatch(receiveUser(data));
         });
     }
