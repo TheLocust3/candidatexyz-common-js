c4bb0bb0c2c4f95927708d836e4b164f4debdfed
TheLocust3 <jake.kinsella@gmail.com>
2018-07-19 15:52:01 -0400

    Add merge donations to donor helper

--- src/helpers/donor-helper.js
+++ src/helpers/donor-helper.js
@@ -29,6 +29,18 @@ let DonorHelper = {
 
     generateDonorsInYear(receipts) {
         return this.generateDonors(_.filter(receipts, (receipt) => { return moment(receipt.dateReceived) > moment().subtract(1, 'year') }));
+    },
+
+    mergeDonations(receipts, inKinds) {
+        let cutReceipts = receipts.map((receipt) => {
+            return { id: receipt.id, type: 'Receipt', name: receipt.name, amount: receipt.amount, amountString: receipt.amountString, dateReceived: receipt.dateReceived, address: receipt.address, city: receipt.city, state: receipt.state, country: receipt.country }
+        });
+
+        let cutInKinds = inKinds.map((inKind) => {
+            return { id: inKind.id, type: 'In Kind', name: inKind.fromWhom, amount: inKind.value, amountString: inKind.valueString, dateReceived: inKind.dateReceived, address: inKind.address, city: inKind.city, state: inKind.state, country: inKind.country }
+        });
+
+        return _.concat(cutReceipts, cutInKinds);
     }
 };
 
