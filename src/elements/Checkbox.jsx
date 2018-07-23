a24c646fa95c13ccaebc13d9ce722ca8a64ee2e0
TheLocust3 <jake.kinsella@gmail.com>
2018-07-23 15:25:37 -0400

    Add input props to checkbox

--- src/elements/Checkbox.jsx
+++ src/elements/Checkbox.jsx
@@ -7,13 +7,13 @@ import Text from './Text';
 export default class Checkbox extends React.Component {
 
     render() {
-        let { className, label, onChange, defaultChecked, ...props } = this.props;
+        let { className, label, onChange, defaultChecked, inputProps, ...props } = this.props;
         className = _.isEmpty(className) ? '' : className;
 
         return (
             <div className='checkbox-wrapper'>
                 <div className={`mdc-checkbox ${className}`} {...props}>
-                    <input type='checkbox' className='mdc-checkbox__native-control' onChange={onChange} defaultChecked={defaultChecked} />
+                    <input type='checkbox' className='mdc-checkbox__native-control' onChange={onChange} defaultChecked={defaultChecked} {...inputProps} />
                     <div className='mdc-checkbox__background'>
                         <svg className='mdc-checkbox__checkmark' viewBox='0 0 24 24'>
                             <path className='mdc-checkbox__checkmark-path' fill='none' stroke='white' d='M1.73,12.91 8.1,19.28 22.79,4.59' />
@@ -34,4 +34,5 @@ Checkbox.propTypes = {
     label: PropTypes.string,
     onChange: PropTypes.func.isRequired,
     defaultChecked: PropTypes.bool,
+    inputProps: PropTypes.object
 };
